f663880e9bf3908ef1c6e142fb820f10a42c3718
Jesus Escalona <eyisus@gmail.com>
2018-05-21 22:40:21 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  if block_given?
+    i = 0
+    newarr = []
+ while i < arr.length
+  if yield(arr[i])
+  newarr << arr[i]
 end
+  i+=1
+ end
+ newarr
+    else
+      "This block should not run!"
+  end
+
+ end
+
